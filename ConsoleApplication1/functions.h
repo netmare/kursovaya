0059bd9a28d7e3d263d6b7fade3632024adc980c
netmare <perec747@gmail.com>
2013-06-04 14:22:05 +0400

    new

--- ConsoleApplication1/functions.h
+++ ConsoleApplication1/functions.h
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-void time(int n, double tn, double tk, double t[]);
+
+#define EXIT_CODE 4
+
+int get_choice(void);
+void time(int n, double tn, double dt, double t[]);
 void uin(int n, double t[], double Uin[]);
 void uout (int n, double t[], double Uin[], double Uout[]);
+double parametr (int n, double t[], double dt, double *U);
+void data_output_screen(int n, double t[], double Uin[], double Uout[]);
+void data_output_file(int n, double t[], double Uin[], double Uout[]);
